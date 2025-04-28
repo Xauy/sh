a6a77303adfa0909586718b0c7eefad88047457c
tralepop <egoricwork@gmail.com>
2025-04-28 16:57:11 +0300

    first commit

---
+++ venv/alien_invasion.py
@@ -0,0 +1,20 @@
+
+import sys
+import pygame
+from settings import Settings
+def run_game():
+	
+ # Инициализирует игру и создает объект экрана.
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+ # Запуск основного цикла игры.
+	while True:
+		screen.fill(ai_settings.bg_color)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		pygame.display.flip()
+run_game()
